c6f98901b78069008bd05774611f02e6b95dd746
paulogrillo <paulodigitalart@gmail.com>
2022-07-01 19:22:09 -0300

    Create UserResponse

---
+++ src/main/java/br/com/showmusic/app/model/UserResponse.java
@@ -0,0 +1,16 @@
+package br.com.showmusic.app.model;
+
+public class UserResponse {
+
+    private String message;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
